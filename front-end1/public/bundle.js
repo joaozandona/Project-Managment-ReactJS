a3d706e9334e6102d93df3a36d36eec60480f047
joaozandona <joaoricardozandona@hotmail.com>
2021-03-09 14:47:34 -0300

    Teste

---
+++ front-end1/public/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\n/home/joaozandona/Documentos/goStack/front-end-ReactJS/babel.config.js:2\\n  presets = [\\n  ^^^^^^^^^^^\\n\\nSyntaxError: Invalid shorthand property initializer\\n    at new Script (vm.js:101:7)\\n    at NativeCompileCache._moduleCompile (/home/joaozandona/Documentos/goStack/front-end-ReactJS/node_modules/v8-compile-cache/v8-compile-cache.js:240:18)\\n    at Module._compile (/home/joaozandona/Documentos/goStack/front-end-ReactJS/node_modules/v8-compile-cache/v8-compile-cache.js:184:36)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:1092:10)\\n    at Module.load (internal/modules/cjs/loader.js:928:32)\\n    at Function.Module._load (internal/modules/cjs/loader.js:769:14)\\n    at Module.require (internal/modules/cjs/loader.js:952:19)\\n    at require (/home/joaozandona/Documentos/goStack/front-end-ReactJS/node_modules/v8-compile-cache/v8-compile-cache.js:159:20)\\n    at loadCjsDefault (/home/joaozandona/Documentos/goStack/front-end-ReactJS/node_modules/@babel/core/lib/config/files/module-types.js:77:18)\\n    at loadCjsOrMjsDefault (/home/joaozandona/Documentos/goStack/front-end-ReactJS/node_modules/@babel/core/lib/config/files/module-types.js:49:16)\");\n\n//# sourceURL=webpack://front-end-ReactJS/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ front-end1/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Header from './components/Header.js';
+
+export default function App(){
+  return (
+            <>
+              <Header />
+              <Header />
+            </>
+          );
+}
