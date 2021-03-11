4625eec8a625fb0e726c8d50d18d8921e5efb2ad
joaozandona <joaoricardozandona@hotmail.com>
2021-03-11 18:42:00 -0300

    Front-end e Back-end integrados, e o Front-end está exibindo os projetos que nós adicionamos no back-end com o Insomnia

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import "./App.css";
 import backgroundImage from "./assets/image1.jpg";
@@ -6,8 +7,14 @@ import backgroundImage from "./assets/image1.jpg";
 import Header from './components/Header.js';
 
 function App(){
-  const [projects, setProjects] = useState(['Desenvolvimento de app',  'Front-end web']);
+  const [projects, setProjects] = useState([]);
   
+  useEffect(() => {
+    api.get('/projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
+
   function handleAddProject(){
 
     setProjects([ ... projects , `Novo Projeto ${Date.now()}`]);
@@ -18,9 +25,8 @@ function App(){
   return (
             <>
               <Header title="Homepage"/>
-              <img width={250} src={backgroundImage}/>
                 <ul>
-                  {projects.map(project => <li key={project}>{project}</li>)}
+                  {projects.map(project => <li key={project.id}>{project.title}</li>)}
 
                   <button type="button" onClick= {handleAddProject}>Adicionar Projeto</button>
                 </ul>
